db3d52d4d2e16a0ddde2a3276bae6570d57e543d
Yelimee <yelimee1004@naver.com>
2015-03-03 01:19:45 +0900

    no error

---
+++ gen/org/androidtown/multimemo/R.java
@@ -0,0 +1,105 @@
+package org.androidtown.multimemo;
+
+public final class R {
+	public static final class anim {
+        public static final int translate_left=0x7f040000;
+        public static final int translate_right=0x7f040001;
+    }
+    public static final class array {
+        public static final int array_handwriting=0x7f050006;
+        public static final int array_handwriting_ex=0x7f050007;
+        public static final int array_photo=0x7f050000;
+        public static final int array_photo_ex=0x7f050001;
+        public static final int array_video=0x7f050002;
+        public static final int array_video_ex=0x7f050003;
+        public static final int array_voice=0x7f050004;
+        public static final int array_voice_ex=0x7f050005;
+    }
+    public static final class attr {
+    }
+    public static final class drawable {
+        public static final int bg_memo=0x7f020000;
+        public static final int btn_camera_capture_click=0x7f020001;
+        public static final int btn_camera_capture_normal=0x7f020002;
+        public static final int handwriting_back=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int icon_video=0x7f020005;
+        public static final int icon_video_empty=0x7f020006;
+        public static final int icon_voice=0x7f020007;
+        public static final int icon_voice_empty=0x7f020008;
+        public static final int memo_back=0x7f020009;
+        public static final int person=0x7f02000a;
+        public static final int person_add=0x7f02000b;
+        public static final int title_background=0x7f02000c;
+        public static final int title_button=0x7f02000d;
+        public static final int title_button_clicked=0x7f02000e;
+    }
+    public static final class id {
+        public static final int buttonLayout=0x7f07000d;
+        public static final int capture_takeBtn=0x7f07001a;
+        public static final int closeBtn=0x7f070018;
+        public static final int contentsLayout=0x7f070002;
+        public static final int frame=0x7f070019;
+        public static final int insert_cancelBtn=0x7f07000f;
+        public static final int insert_dateBtn=0x7f07000c;
+        public static final int insert_handwriting=0x7f070009;
+        public static final int insert_handwritingBtn=0x7f07000b;
+        public static final int insert_memoEdit=0x7f070008;
+        public static final int insert_photo=0x7f070004;
+        public static final int insert_saveBtn=0x7f07000e;
+        public static final int insert_textBtn=0x7f07000a;
+        public static final int insert_videoBtn=0x7f070005;
+        public static final int insert_voiceBtn=0x7f070006;
+        public static final int itemDate=0x7f070011;
+        public static final int itemHandwriting=0x7f070015;
+        public static final int itemPhoto=0x7f070010;
+        public static final int itemText=0x7f070012;
+        public static final int itemVideoState=0x7f070013;
+        public static final int itemVoiceState=0x7f070014;
+        public static final int loading_cancelBtn=0x7f070020;
+        public static final int loading_gallery=0x7f07001b;
+        public static final int loading_okBtn=0x7f07001f;
+        public static final int loading_selectPhotoText=0x7f07001c;
+        public static final int loading_selectedPhoto=0x7f07001d;
+        public static final int mediaLayout=0x7f070003;
+        public static final int memoLayout=0x7f070007;
+        public static final int memoList=0x7f070016;
+        public static final int newMemoBtn=0x7f070017;
+        public static final int sub_layout01=0x7f07001e;
+        public static final int titleBackgroundBtn=0x7f070001;
+        public static final int titleLayout=0x7f070000;
+    }
+    public static final class layout {
+        public static final int memo_insert_activity=0x7f030000;
+        public static final int memo_listitem=0x7f030001;
+        public static final int multimemo=0x7f030002;
+        public static final int photo_capture_activity=0x7f030003;
+        public static final int photo_selection_activity=0x7f030004;
+    }
+    public static final class string {
+        public static final int app_name=0x7f060001;
+        public static final int hello=0x7f060000;
+    }
+    public static final class styleable {
+        /**  These are the attributes that we want to retrieve from the theme
+         in view/Gallery1.java 
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #Gallery1_android_galleryItemBackground org.androidtown.multimemo:android_galleryItemBackground}</code></td><td></td></tr>
+           </table>
+           @see #Gallery1_android_galleryItemBackground
+         */
+        public static final int[] Gallery1 = {
+            0x0101004c
+        };
+        /**
+          <p>This symbol is the offset where the {@link org.androidtown.multimemo.R.attr#android_galleryItemBackground}
+          attribute's value can be found in the {@link #Gallery1} array.
+          @attr name android:android_galleryItemBackground
+        */
+        public static final int Gallery1_android_galleryItemBackground = 0;
+    };
+}
